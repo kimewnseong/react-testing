3b60f9eac6076b0f6786a2e6bf6df7410ebc4f4d
kimewnseong <rladbstjd96@naver.com>
2023-09-16 18:18:43 +0900

    mouse hover test
    보이지 않는 요소 테스트

--- my-code/sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
+++ my-code/sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -1,4 +1,6 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
+
 import SummaryForm from '../SummaryForm';
 
 test('Initial conditions', () => {
@@ -13,7 +15,9 @@ test('Initial conditions', () => {
   expect(confirmButton).toBeDisabled();
 });
 
-test('체크박스 첫 클릭 시 버튼 활성화 및 두번 클릭 시 버튼 비활성', () => {
+test('체크박스 첫 클릭 시 버튼 활성화 및 두번 클릭 시 버튼 비활성', async () => {
+  const user = userEvent.setup();
+
   render(<SummaryForm />);
 
   const checkbox = screen.getByRole('checkbox', {
@@ -21,10 +25,32 @@ test('체크박스 첫 클릭 시 버튼 활성화 및 두번 클릭 시 버튼 
   });
   const confirmButton = screen.getByRole('button', { name: /confirm order/i });
 
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(confirmButton).toBeEnabled();
 
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(checkbox).not.toBeChecked();
   expect(confirmButton).toBeDisabled();
+});
+
+test('마우스 커서를 올리면 팝오버 호출', async () => {
+  const user = userEvent.setup();
+
+  render(<SummaryForm />);
+  // 보이지 않는 요소를 테스트할 경우 getBy를 사용해서는 안됨
+  // 팝오버가 감춰졌을 때
+  const nullPopOver = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopOver).not.toBeInTheDocument();
+
+  // 체크박스 라벨에 마우스가 올라갔을 때
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  await user.hover(termsAndConditions);
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+
+  // 마우스가 떠났을 때
+  await user.unhover(termsAndConditions);
+  expect(popover).not.toBeInTheDocument();
 });
