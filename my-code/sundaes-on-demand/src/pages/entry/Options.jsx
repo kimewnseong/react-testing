8b60e7a3cce62859dda41b94abfc279e7dd7358d
kimewnseong <rladbstjd96@naver.com>
2023-09-17 14:45:53 +0900

    이미지 test case 추가

--- my-code/sundaes-on-demand/src/pages/entry/Options.jsx
+++ my-code/sundaes-on-demand/src/pages/entry/Options.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import Row from 'react-bootstrap/Row';
 
 import ScoopOption from './ScoopOption';
+import ToppingOption from './ToppingOption';
 
 function Options({ optionType }) {
   const [items, setItems] = useState([]);
@@ -16,7 +17,7 @@ function Options({ optionType }) {
       });
   }, [optionType]);
 
-  const ItemComponent = optionType === 'scoops' ? ScoopOption : null;
+  const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
 
   const optionItems = items.map((item) => (
     <ItemComponent
--- my-code/sundaes-on-demand/src/pages/entry/tests/Options.test.jsx
+++ my-code/sundaes-on-demand/src/pages/entry/tests/Options.test.jsx
@@ -12,3 +12,21 @@ test('각 scoop옵션의 이미지가 표시되는가', async () => {
   const altText = scoopImages.map((el) => el.alt);
   expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
 });
+
+test('각 topping옵션의 이미지가 표시되는가', async () => {
+  render(<Options optionType="toppings" />);
+
+  // 이미지 찾기
+  const toppingImages = await screen.findAllByRole('img', {
+    name: /topping$/i,
+  });
+  expect(toppingImages).toHaveLength(3);
+
+  // 이미지의 alt 텍스트 확인
+  const altText = toppingImages.map((el) => el.alt);
+  expect(altText).toEqual([
+    'Cherries topping',
+    'M&Ms topping',
+    'Hot fudge topping',
+  ]);
+});
